358f290e9ee493fc79092b9c60d31641b29884fa
unknown <kingbj0429@gmail.com>
2021-03-23 09:10:04 +0900

    fix : 2d_list_init.py

--- 2d_list/2d_list_init.py
+++ 2d_list/2d_list_init.py
@@ -8,3 +8,6 @@ print(input_list)
 
 #input_list = [list(input()) for _ in range(N)] 문자열 한줄씩 리스트에 담기
 #input_list = [list(map(int,input())) for _ in range(N)] 문자열 한줄씩 리스트에 담기
+
+
+#input_data = [list(input()) for _ in range(N)] ===========> [['A', 'A', 'B'], ['A', 'C', 'D'], ['B', 'D', 'E']]
--- test2.py
+++
@@ -1,7 +0,0 @@
-N = int(input())
-
-input_list = [list(input()) for _ in range(N)]
-
-result = list(input_list[0])
-
-print(result[0])
